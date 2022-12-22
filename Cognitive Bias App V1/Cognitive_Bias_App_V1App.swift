deb729adf8e42624e4c0e8d01feb42f024beb2db
JohnC7777 <john.citrowske@gmail.com>
2022-12-21 18:59:01 -0600

    Initial Commit

---
+++ Cognitive Bias App V1/Cognitive_Bias_App_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Cognitive_Bias_App_V1App.swift
+//  Cognitive Bias App V1
+//
+//  Created by user230876 on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Cognitive_Bias_App_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cognitive Bias App V1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cognitive Bias App V1
+//
+//  Created by user230876 on 12/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
